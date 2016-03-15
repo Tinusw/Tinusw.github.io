463b92e538ec406b2d80abd0809fd25ba8246240
Tinus <vertelmy@gmail.com>
2016-03-16 00:43:18 +0100

    tons of image compression.

--- js/site.js
+++ js/site.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-
-
       // Smooth Scrolling
-      // var $root = $('html, body');
-      //   $('.navbar ul a').click(function() {
-      //       var href = $.attr(this, 'href');
-      //       $root.animate({
-      //           scrollTop: $(href).offset().top
-      //       }, 500, function () {
-      //           window.location.hash = href;
-      //       });
-      //       return false;
-      //   });   
+      var $root = $('html, body');
+        $('.navbar ul a').click(function() {
+            var href = $.attr(this, 'href');
+            $root.animate({
+                scrollTop: $(href).offset().top
+            }, 500, function () {
+                window.location.hash = href;
+            });
+            return false;
+        });   
 });
