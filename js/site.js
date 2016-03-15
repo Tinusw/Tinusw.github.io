04cb5639dc4c106cf407f663ad096d792eea2801
Tinus <vertelmy@gmail.com>
2016-03-15 23:49:52 +0100

    nightly commit

--- js/site.js
+++ js/site.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
-  
-  // Smooth Scrolling
-  var $root = $('html, body');
-    $('.navbar ul a').click(function() {
-        var href = $.attr(this, 'href');
-        $root.animate({
-            scrollTop: $(href).offset().top
-        }, 500, function () {
-            window.location.hash = href;
-        });
-        return false;
-    });
 
-   
+
+      // Smooth Scrolling
+      // var $root = $('html, body');
+      //   $('.navbar ul a').click(function() {
+      //       var href = $.attr(this, 'href');
+      //       $root.animate({
+      //           scrollTop: $(href).offset().top
+      //       }, 500, function () {
+      //           window.location.hash = href;
+      //       });
+      //       return false;
+      //   });   
 });
