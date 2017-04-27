13c05b8365578a9c6ad8600c9e762530087a6380
Tinus Wagner <tinus@3months.com>
2017-04-27 21:58:54 +1200

    basic webpack config complete

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./src/entry.js",
+    output: {
+        path: __dirname,
+        filename: "./bundle.js"
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader"
+            }]
+        }]
+    }
+};
