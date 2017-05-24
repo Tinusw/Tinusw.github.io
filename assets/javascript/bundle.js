86af83694d98f31613dc744a97a571113fcf5cf2
Tinus Wagner <tinus@3months.com>
2017-05-24 22:19:09 +1200

    got a bit of a background animation going

--- assets/javascript/bundle.js
+++ assets/javascript/bundle.js
@@ -1,2 +1,5 @@
-// @codekit-append "utilities.js"
 // @codekit-append "typed.js"
+// @codekit-append "utilities.js"
+// @codekit-append "TweenLite.min.js"
+// @codekit-append "EasePack.min.js"
+// @codekit-append "rAF.js"
---
+++ assets/javascript/utilities.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function(){
+    Typed.new(".title", {
+      strings: ["Software Engineer", "Frontend Developer", "Backend Developer", "Ruby on Rails", "SASS", "Javascript", "jQuery", "HTML", "HAML", "Scrum Master", "Active Record", "PostgreSQL", "NodeJS", "Webpack", "D3.js", "Express", "React", "Responsive Design", "Musician", "Cat Lover"],
+      typeSpeed: 80,
+      backDelay: 1000,
+      loop: true
+    });
+});
