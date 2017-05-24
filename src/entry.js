37c36d9a57bb51931fa46062db9000d580505b1e
Tinus Wagner <tinus@3months.com>
2017-05-24 20:16:05 +1200

    let us start over

--- src/entry.js
+++
@@ -1,5 +0,0 @@
-// import $ from "jquery";
-require('./scss/app.scss');
-require('bootstrap-loader');
-require('./typed.js');
-require('./utilities.js');
--- src/utilities.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-	Typed.new(".title", {
-		strings: ["Front End Code.", "Back End Code.", "Ruby on Rails", "Javascript", "Agile Teams", "inter-disciplinary teams", "Styling", "Web Audio", "Web Applications"],
-		typeSpeed: 150
-	});
-});
