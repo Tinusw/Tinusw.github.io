3fc76df3a9fdb79c2330c06b7b0029ed6600dc80
Tinusw <tinus@3months.com>
2018-03-22 12:45:33 +0100

    update cv

--- assets/javascript/utilities.js
+++ assets/javascript/utilities.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function(){
     Typed.new(".title", {
-      strings: ["Software Engineer", "Frontend Developer", "Backend Developer", "Ruby on Rails", "SASS", "Javascript", "jQuery", "HTML", "HAML", "Scrum Master", "Active Record", "PostgreSQL", "NodeJS", "Webpack", "D3.js", "Express", "React", "Responsive Design", "Musician", "Cat Lover"],
+      strings: ["Software Engineer", "Frontend Developer", "Backend Developer", "Ruby on Rails", "SASS", "Javascript", "Pug", "HTML", "HAML", "Scrum Master", "Active Record", "PostgreSQL", "NodeJS", "Webpack", "D3.js", "Express", "React", "Responsive Design", "Musician", "Cat Lover"],
       typeSpeed: 80,
       backDelay: 1000,
       loop: true
